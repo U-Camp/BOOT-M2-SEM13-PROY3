e89054d0a61e9eeaf62dece8c1a3f089e7cf1991
Mike Nieva <m@mikenieva.com>
2021-04-30 09:32:43 -0500

    Desarrollo de guión

---
+++ demo/index.js
@@ -0,0 +1,27 @@
+var ctx = document.getElementById('myChart').getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: ['Enero', 'Febreri', 'Marzo', 'Abril', "Mayo"],
+        datasets: [{
+            label: 'Ventas 2021',
+            data: [12, 10, 3, 5, 2, 3, 20],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 3
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
